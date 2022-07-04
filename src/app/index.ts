474832ce77d2243c406abe73d87b1c23564a4d05
Adam Teodoro <adamteodoronunes@gmail.com>
2022-07-04 00:33:05 -0300

    install minhtml,update config

--- src/app/index.ts
+++ src/app/index.ts
@@ -2,37 +2,73 @@
 
 import * as fs from 'fs';
 import * as qrcode from 'qrcode';
+import * as htmlMinifier from 'html-minifier';
+import * as uglyfyJs from 'uglify-js';
 
-import { Command } from 'commander';
 import { readFile } from 'fs/promises';
+import { Command } from 'commander';
+
+import qrs_config from './qrs_config';
+
+import { IMinConfig } from './interfaces/IMinConfig';
 
 const lzw = require('lzw');
 
 //json format
 const command = new Command();
 command.version('1.0.0');
-const strConfig = `{
-    "input_path": "",
-    "output_path": "",
-    "isMin": false
-}
-`;
 
 //validator from error 
 const validator = {
+    fileExists: (filePath: string) => {
+        if (fs.existsSync(filePath)) {
+            return true;
+        } else {
+            return false;
+        }
+    },
+    fileNotExists: (filePath: string) => {
+        return !validator.fileNotExists(filePath);
+    },
+    errorIfFileExists: (filePath: string) => {
+        if (validator.fileExists(filePath)) {
+            const filePathSplit = filePath.split('/');
+            const fileNameAndExtension = filePathSplit[filePathSplit.length - 1];
+            const fileName = fileNameAndExtension.split('.')[0]
+            const msg = `The file '${fileName}' already exists!`;
+            throw new Error(msg);
+        }
+    },
+    errorIfFileNotExists: (filePath: string) => {
+        if (validator.fileNotExists(filePath)) {
+            const filePathSplit = filePath.split('/');
+            const fileNameAndExtension = filePathSplit[filePathSplit.length - 1];
+            const fileName = fileNameAndExtension.split('.')[0]
+            const msg = `The file '${fileName}' already exists!`;
+            throw new Error(msg);
+        }
+    },
     validateFileExtension: (fileName: string, expected: string) => {
         const extensionReceived = fileName.split('.')[1];
-        if (extensionReceived === expected) {
-            return;
-        } else {
+        if (extensionReceived !== expected) {
             const msg = `Invalid file type, expected: '${expected}'`;
             throw new Error(msg);
         }
+        return;
     }
 }
 
 const qrsService = {
-    codeMin: (htmlFileStr: string) => {
+    getIndexListOf(str: string, match: string): Array<number> {
+        const indexList: Array<number> = [];
+        for (let i = 0; i < str.length; i++) {
+            if (str.substring(i, i + match.length) == match) {
+              indexList.push(i);
+            }
+        }
+        return indexList;
+    },
+    codeReplaced: (htmlFileStr: string) => {
         let replacedCode = htmlFileStr;
         replacedCode.replace('¤', '¤¤');
         replacedCode.replace('<script>', '¤ts');
@@ -56,7 +92,7 @@ const qrsService = {
         replacedCode.replace(' style', '¤ps');
         replacedCode.replace('hidden', '¤ph');
         replacedCode.replace('type', '¤pt');
-        replacedCode.replace('class=', '¤pc');
+        replacedCode.replace('class', '¤pc');
         //js
         replacedCode.replace('alert(', '¤ja');
         replacedCode.replace('function', '¤jf');
@@ -70,38 +106,49 @@ const qrsService = {
         const finalPath = splitPath[0] + `_${index}.${splitPath[1]}`;
         return finalPath;
     },
-    minify: () => {
-        
+    minifyJs: (htmlStr: string) => {
+        const qtdScript = htmlStr.split('<script>').length;
+        for (let i = 0; i < qtdScript; i++) {
+            const indexListOpenScript = qrsService.getIndexListOf(htmlStr, '<script>');
+            const indexListCloseScript = qrsService.getIndexListOf(htmlStr, '</script>')
+            const js = htmlStr.indexOf('<script>', i)
+        }
+
+    },
+    minify: async (input_path: string, minify_config: IMinConfig) => {
+        const { minify_code, minify_options } = minify_config;
+        if (minify_code) {
+            const buffer = await readFile(input_path);
+            const htmlStr = buffer.toString();
+            const htmlStrMin = htmlMinifier.minify(htmlStr, minify_options);
+            qrsService.minifyJs(htmlStrMin)
+            return htmlStrMin;
+        } else {
+            const buffer = await readFile(input_path);
+            const htmlStr = buffer.toString();
+            return htmlStr;
+        }
     }
 }
 
-//cli functions qrss
+//cli functions qrs
 const qrs = {
     //init configuration
     'init': () => {
-        try {
-            fs.writeFileSync('./qrs-config.json', strConfig);
-        } catch(error) {
-            console.log('error', error);
-            throw new Error(error);
-        }
+        validator.errorIfFileExists('./qrs-config.json');
+        fs.writeFileSync('./qrs-config.json', qrs_config);
     },
     'build': async () => {
         const qrsConfigFile = fs.readFileSync('./qrs-config.json');
-        const qrsConfig: { 
+        const qrsConfig: {
             input_path: string,
-            output_path: string
+            output_path: string,
+            minify_config: IMinConfig;
         } = JSON.parse(qrsConfigFile.toString());
-        const { input_path, output_path } = qrsConfig;
+        const { input_path, output_path, minify_config } = qrsConfig;
         validator.validateFileExtension(input_path, 'html');
-
-
-
-
-
-        const buffer = await readFile(input_path);
-        const htmlFileStr = buffer.toString();
-        let compressed = lzw.compress(qrsService.codeMin(htmlFileStr));
+        let htmlFileStr = await qrsService.minify(input_path, minify_config);
+        let compressed = lzw.compress(qrsService.codeReplaced(htmlFileStr));
         const strCompressed: string = compressed.toString();
         const iterations = strCompressed.length / 2364;
         let strStart = 0;
@@ -110,7 +157,7 @@ const qrs = {
         for (let i = 0; i < iterations; i++) {
             strFinal = strFinal + 2364;
             const strSplit = strCompressed.substring(strStart, strFinal);
-            strSplitList.push(( i + 1) + '-' + strSplit);
+            strSplitList.push(strSplit);
             strStart = strFinal;
         }
         if (strCompressed.length % iterations !== 0) {
@@ -120,6 +167,11 @@ const qrs = {
             );
             strSplitList.push(strSplit);
         }
+        strSplitList.forEach((strSplit, index) => {
+            const strDroped = strSplitList.splice(index);
+            //add 'index of qrcode/number of qrcode-' in init of str 
+            strSplitList.push(`${index}/${strSplit.length}-` + strDroped);
+        });
         strSplitList.forEach(async (strSplit, index) => {
             const finalPath = qrsService.addIndexToPath(output_path, index);
             await qrcode.toFile(
@@ -129,13 +181,12 @@ const qrs = {
                     type: 'png',
                     version: 40,
                     scale: 2,
-                    width: 177,
+                    width: 1980,
                     errorCorrectionLevel: 'L',
                 }
             );
         });
-    },
-    'reverse': () => {}
+    }
 }
 //
 command.command('qrs [option]')
---
+++ src/app/interfaces/IMinConfig.ts
@@ -0,0 +1,6 @@
+import  { Options } from 'html-minifier';
+
+export interface IMinConfig {
+    minify_code: boolean,
+    minify_options: Options
+}
